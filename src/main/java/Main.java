5e270ad18cedd7fbf53b466aa1b2eb88cfc3f645
narcissusTheFlower <narcissustheflower2047@gmail.com>
2024-06-01 18:59:02 +0300

    Changes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,20 @@
 import java.nio.file.Path;
 
+
 public class Main {
-// /home/user/IdeaProjects/test/tickets.json
+
     public static void main(String[] args){
 
         if (args.length == 0){
             System.out.println("Point jar to the ticket.json file for it to work!");
             return;
         }
+
         //Implement proper builder with inner class
         TicketParser.of(Path.of(args[0]))
             .parseJson()
             .calculate()
             .printResults();
-
     }
 
 }
--- src/main/java/TicketParser.java
+++ src/main/java/TicketParser.java
@@ -1,8 +1,14 @@
 import java.io.IOException;
 import java.nio.file.Path;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.AbstractMap;
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
@@ -11,32 +17,52 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 class TicketParser {
 
     /**
-     * Средняя цена для полета между владивостоком и тель-авивом =  (12400 + 13100 + 15300 + 11000 + 13400 + 12450 + 13600 + 14250 + 16700 + 17400) = 13960
-     *
-     * ("departure_date": "12.05.18" + "departure_time": "16:20") (epoch seconds) -  (epoch seconds) ("arrival_date": "12.05.18" +  "arrival_time": "22:10") = 8 часов 30 минут
+     * JSON file path.
      */
     private final Path filePath;
 
+    /**
+     * Ticket only of VVO to TLV.
+     */
     private Collection<OneTicketDTO> parsedTickets;
 
+    /**
+     * Text representation of the answer.
+     */
     private Result result;
 
+    /**
+     * Private constructor that does not allow usage of the "new" keyword.
+     * @param filePath
+     */
     private TicketParser(Path filePath) {
         this.filePath = filePath;
     }
 
+    /**
+     * Protected factory
+     * @param filePath
+     * @return new instance of this class.
+     */
     static TicketParser of(Path filePath) {
         return new TicketParser(filePath);
     }
 
+    /**
+     * Parses only TLV to VVO tickets from the file
+     * @return this instance.
+     */
     TicketParser parseJson() {
         ObjectMapper objectMapper = new ObjectMapper();
         try {
             TicketsDTO dto = objectMapper.readValue(filePath.toFile(), TicketsDTO.class);
-            parsedTickets = dto.getTickets().stream()
-                    .filter(ticket -> ticket.getOrigin().equals("VVO") && ticket.getDestination().equals("TLV"))
-                    .sorted()
-                    .toList();
+            parsedTickets = dto.getTickets()
+                .stream()
+                .filter(ticket -> ticket.getOrigin()
+                    .equals("VVO") && ticket.getDestination()
+                    .equals("TLV"))
+                .sorted()
+                .toList();
         } catch (IOException e) {
             System.out.println("Failed to parse the ticket.json file!");
             throw new RuntimeException(e);
@@ -45,15 +71,24 @@ class TicketParser {
     }
 
     /**
-     * Calculate minimal price and difference between average price and the median one
-     * @return
+     * Calculates minimal price and difference between average price and the median one
+     * @return this instance.
      */
     TicketParser calculate() {
-        double priceAverage = parsedTickets.stream().mapToDouble(OneTicketDTO::getPrice).average().orElse(0);
-        double priceMedian = calculateMedian(parsedTickets.stream().map(OneTicketDTO::getPrice).toList());
-        double priceDifference = Math.abs(priceAverage-priceMedian);
-        Map<String,String> times= null; //COUNT TIMES
-        result = Result.from(Collections.EMPTY_MAP,priceAverage,priceMedian,priceDifference);
+        double priceAverage = parsedTickets.stream()
+            .mapToDouble(OneTicketDTO::getPrice)
+            .average()
+            .orElse(0);
+        double priceMedian = calculateMedian(parsedTickets.stream()
+            .map(OneTicketDTO::getPrice)
+            .toList());
+        //For readability only. Variable could be removed
+        double priceDifference = priceAverage - priceMedian;
+
+        result = Result.from(calculateMinimalPriceForEachCarrier(parsedTickets),
+            priceAverage,
+            priceMedian,
+            priceDifference);
         return this;
     }
 
@@ -61,23 +96,82 @@ class TicketParser {
         System.out.println(result);
     }
 
-    //FINISH MEDIAN
-    private <T extends Number> T calculateMedian(List<T> values){
-        if (values.size() % 2 != 0){
-            return (double) values.size() /2;
+    /**
+     * Get the median of the prices
+     * @param values
+     * @return median of the ticket prices
+     */
+    private double calculateMedian(List<Double> values) {
+        if (values.size() % 2 != 0) {
+            return values.get(values.size() / 2);
         }
-//
-//        Arrays.sort(numArray);
-//        double median;
-//        if (numArray.length % 2 == 0)
-//            median = ((double)numArray[numArray.length/2] + (double)numArray[numArray.length/2 - 1])/2;
-//        else
-//            median = (double) numArray[numArray.length/2];
-
-        return (T) new Double(1);
+        return (values.get(values.size() / 2) + values.get(values.size() / 2 - 1)) / 2;
     }
 
-    private static class Result{
+    /**
+     * Calculate time difference between two dates in minutes.
+     * @param departureTime
+     * @param arrivalTime
+     * @return minutes between two dates.
+     */
+    private long getTimeDifference(String departureTime, String arrivalTime) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yy HH:mm");
+        try {
+            Date departureDate = sdf.parse(departureTime);
+            Date arrivalDate = sdf.parse(arrivalTime);
+            return (Duration.ofMillis(arrivalDate.getTime() - departureDate.getTime())
+                .toMinutes());
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Calculates and assembles representation of minimal flight time for each distinct carrier.
+     * @param parsedTickets
+     * @return minimal flight time for each distinct carrier
+     */
+    private Map<String, String> calculateMinimalPriceForEachCarrier(Collection<OneTicketDTO> parsedTickets) {
+        HashSet<AbstractMap.SimpleEntry<String, Collection<Long>>> temp = new HashSet<>();
+
+        parsedTickets.forEach(ticket -> temp.add(new AbstractMap.SimpleEntry<>(ticket.getCarrier(), new ArrayList<Long>())));
+
+        temp.forEach(distinctCarrier -> {
+            parsedTickets.stream()
+                .filter(ticket -> ticket.getCarrier()
+                    .equals(distinctCarrier.getKey()))
+                .forEach(filteredTicket -> distinctCarrier.getValue()
+                    .add(getTimeDifference(filteredTicket.getDepartureDate() + " " + filteredTicket.getDepartureTime(),
+                        filteredTicket.getArrivalDate() + " " + filteredTicket.getArrivalTime())));
+        });
+        Map<String, String> result = new HashMap<>();
+        temp.forEach(pair -> {
+            result.put(pair.getKey(), timeAnswerBuilder(pair.getValue()
+                .stream()
+                .sorted()
+                .findFirst()
+                .get())
+            );
+        });
+        return result;
+    }
+
+    /**
+     * Convenience method for String building.
+     * @param minutes
+     * @return readable String answer parsed from minutes.
+     */
+    private String timeAnswerBuilder(Long minutes) {
+        return new StringBuilder()
+            .append("(hours:" + minutes / 60)
+            .append(" minutes:" + minutes % 60 + ")")
+            .toString();
+    }
+
+    /**
+     * Answer representation.
+     */
+    private static class Result {
 
         private Map<String, String> times;
         private double priceAverage;
@@ -91,24 +185,28 @@ class TicketParser {
             this.priceDifference = priceDifference;
         }
 
-        public static Result from(Map<String, String> times, double priceAverage, double priceMedian, double priceDifference){
-            return new Result(times,priceAverage,priceMedian,priceDifference);
+        public static Result from(Map<String, String> times, double priceAverage, double priceMedian, double priceDifference) {
+            return new Result(times, priceAverage, priceMedian, priceDifference);
         }
 
         @Override
         public String toString() {
-            return new StringBuilder()
-                .append("Минимальное время полета между городами:")
+            return new StringBuilder().append("Минимальное время полета между городами:")
                 .append(times)
                 .append("\n")
-                .append("Среднее значение цены: ").append(priceAverage)
                 .append("\n")
-                .append("Медиана цены: ").append(priceMedian)
+                .append("Значения цен полета между городами Владивосток и Тель-Авив")
                 .append("\n")
-                .append("Разница среднего значения и медианы цены: ").append(priceDifference)
+                .append("Среднее значение цены за билет: ")
+                .append(priceAverage)
+                .append("\n")
+                .append("Медиана цены: ")
+                .append(priceMedian)
+                .append("\n")
+                .append("Разница среднего значения и медианы: ")
+                .append(priceDifference)
                 .toString();
         }
     }
-
 
 }
